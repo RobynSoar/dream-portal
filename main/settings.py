e69131d90578c69260e9618634f35ac6b28ff7b1
Robyn Soar <robyn999@hotmail.co.uk>
2025-01-18 17:03:14 +0000

    Create new secret key within ignored file

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eisb*o)f^$c9hv!^ekvt=(x4a(akyh#2b4l%wyod#t&$*02v2p'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
