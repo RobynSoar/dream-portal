64630bccf111fe1635103c1fec084f33b3961a55
Robyn Soar <robyn999@hotmail.co.uk>
2025-01-20 15:53:44 +0000

    Install gunicorn for deployment to Heroku

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['8000-robynsoar-dreamportal-uwgfwgbt33i.ws-eu117.gitpod.io']
+ALLOWED_HOSTS = ['8000-robynsoar-dreamportal-uwgfwgbt33i.ws-eu117.gitpod.io','.herokuapp.com']
 
 
 # Application definition
