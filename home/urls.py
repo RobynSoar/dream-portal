c64cb4ecc9033be1854cef4c597530f80372913b
Robyn Soar <robyn999@hotmail.co.uk>
2025-01-21 20:30:58 +0000

    Add generic postlist test

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dream_home, name='home')
+    # path('', views.dream_home, name='home')
+    path('', views.PostList.as_view(), name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,6 +4,8 @@ It includes the logic to render templates and handle requests.
 """
 
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 
 # Create your views here.
@@ -12,3 +14,8 @@ def dream_home(request):
     Renders the 'base.html' template for the Home application.
     """
     return render(request, 'base.html')
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
